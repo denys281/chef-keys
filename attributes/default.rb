a58cbcc8260318b2412f9e5772a35331bd1f7a8e
Pim Snel <pim@lingewoud.nl>
2013-03-30 00:44:59 +0100

    make work for me

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,5 @@
+default[:username] = "vagrant"
+default[:usergroup] = "vagrant"
+default[:userhomedir] = "/home/vagrant"
 default[:keys][:databag] = "my_keys_databag"
 default[:keys][:key_name] = "my_key"
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,17 +1,21 @@
-dir = "/home/chef/.ssh"
+#p node['userhomedir']
+dir = "#{node['ssh_keys']['userhomedir']}/.ssh"
 
 directory dir do
-  owner "chef"
-  group "chef"
+  owner node['ssh_keys']['username']
+  group node['ssh_keys']['usergroup']
   action :create
 end
 
-data_bag_item( node[:keys][:databag], node[:keys][:key_name] ).each do |k, v|
+p node['ssh_keys']
+p dir
+node['ssh_keys'].each do |k, v|
   next unless k.match(/^id_/)
+  p k
   
   file "#{dir}/#{k}" do
-    owner "chef"
-    group "chef"
+    owner node['ssh_keys']['username']
+    group node['ssh_keys']['usergroup']
     mode k.match(/\.pub$/) ? "644" : "600"
     content v
   end
